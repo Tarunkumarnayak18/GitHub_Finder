6d9cbf19121e1569d459de8604725f4972ff9627
Tarunkumarnayak18 <nayaktarun737@gmail.com>
2024-06-20 15:49:29 +0530

    Github account finder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [data, setData] = useState({});
+
+  function handleSearch() {
+    console.log(user);
+    // useEffect(() => {
+    fetch(`https://api.github.com/users/${user}`)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+    // }, [user]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub Account Finder</h1>'
+      <input
+        type="text"
+        placeholder="Enter GitHub username"
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <div>
+        <h1>{data.login}</h1>
+        <p>{data.bio}</p>
+        <img src={data.avatar_url} alt={data.login} />
+      </div>
     </div>
   );
 }
